ed14661f592cc2ebcc885fa2528efbc80e671392
Angel Navas <angelnavasramos@gmail.com>
2020-08-11 01:17:27 -0400

    Add tailwindcss, create a Layout and a Sidebar

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+function Index() {
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Clients</h1>
+    </Layout>
+  )
+}
+
+export default Index;
---
+++ client/pages/orders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+function Orders() {
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Orders</h1>
+    </Layout>
+  )
+}
+
+export default Orders;
