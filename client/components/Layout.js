d04c4685a0d0e3e1f13d007d671a17de20bef0ad
Angel Navas <angelnavasramos@gmail.com>
2020-08-12 03:41:06 -0400

    Add login form

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -21,7 +21,7 @@ function Layout({ children }) {
         />
       </Head>
 
-      {router.pathname === '/login' || '/newaccount' ? (
+      {router.pathname === '/login' || router.pathname === '/newaccount' ? (
         <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
           <div>{children}</div>
         </div>
