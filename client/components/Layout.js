1744087630e7d426ceb067cb79e42e4ca32d3be7
Angel Navas <angelnavasramos@gmail.com>
2020-08-11 23:01:29 -0400

    Add login and newaccount page. Add dynamic layout and next/router link.

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import Sidebar from './Sidebar';
 
-function Layout({children}) {
+function Layout({ children }) {
+  //Routing hook
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>CRM - Clients</title>
-        
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+        />
       </Head>
-      
-      <div className="bg-gray-200 min-h-screen">
-        <div className="flex min-h-screen">
-          <Sidebar />
-          
-          <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
-            {children}
-          </main>
+
+      {router.pathname === '/login' || '/newaccount' ? (
+        <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+          <div>{children}</div>
         </div>
-      </div>
+      ) : (
+        <div className="bg-gray-200 min-h-screen">
+          <div className="flex min-h-screen">
+            <Sidebar />
+
+            <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+              {children}
+            </main>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
 export default Layout;
---
+++ client/pages/newaccount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function NewAccount() {
+  return (
+    <>
+      <Layout>
+        <h1>
+          New Account
+        </h1>
+      </Layout>
+    </>
+  )
+}
+
+export default NewAccount;
