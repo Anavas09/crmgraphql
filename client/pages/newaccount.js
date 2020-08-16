7636f8ee8829258563d8b3a6dd0992b2f229f763
Angel Navas <angelnavasramos@gmail.com>
2020-08-15 21:44:58 -0400

    Add a new component, NewAccountForm. Move all form from newaccount page to the NewAccountForm component.

--- client/pages/newaccount.js
+++ client/pages/newaccount.js
@@ -5,6 +5,7 @@ import * as Yup from 'yup';
 import { useMutation, gql } from '@apollo/client';
 
 import Layout from '../components/Layout';
+import NewAccountForm from '../components/NewAccountForm';
 
 const NEW_ACCOUNT = gql`
   mutation newUser($input: UserInput) {
@@ -85,36 +86,6 @@ function NewAccount() {
     },
   });
 
-  const errorName = formik.touched.name && formik.errors.name && (
-    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-      <p className="font-bold">{formik.errors.name}</p>
-    </div>
-  );
-
-  const errorLastname = formik.touched.lastname && formik.errors.lastname && (
-    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-      <p className="font-bold">{formik.errors.lastname}</p>
-    </div>
-  );
-
-  const errorAge = formik.touched.age && formik.errors.age && (
-    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-      <p className="font-bold">{formik.errors.age}</p>
-    </div>
-  );
-
-  const errorEmail = formik.touched.email && formik.errors.email && (
-    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-      <p className="font-bold">{formik.errors.email}</p>
-    </div>
-  );
-
-  const errorPassword = formik.touched.password && formik.errors.password && (
-    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-      <p className="font-bold">{formik.errors.password}</p>
-    </div>
-  );
-
   const showMessage = () => {
     return (
       good ? (
@@ -141,118 +112,10 @@ function NewAccount() {
 
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form
-              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
-              onSubmit={formik.handleSubmit}
-            >
-              <div className="mb-4">
-                <label
-                  className="block text-green-700 text-sm font-bold mb-2"
-                  htmlFor="name"
-                >
-                  Name
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="name"
-                  type="text"
-                  placeholder="Name"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </div>
-
-              {errorName}
-
-              <div className="mb-4">
-                <label
-                  className="block text-green-700 text-sm font-bold mb-2"
-                  htmlFor="lastname"
-                >
-                  Last Name
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="lastname"
-                  type="text"
-                  placeholder="Last Name"
-                  value={formik.values.lastname}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </div>
-
-              {errorLastname}
-
-              <div className="mb-4">
-                <label
-                  className="block text-green-700 text-sm font-bold mb-2"
-                  htmlFor="age"
-                >
-                  Age
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="age"
-                  type="number"
-                  placeholder="Age"
-                  value={formik.values.age}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </div>
-
-              {errorAge}
-
-              <div className="mb-4">
-                <label
-                  className="block text-green-700 text-sm font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="email"
-                  type="email"
-                  placeholder="User Email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </div>
-
-              {errorEmail}
-
-              <div className="mb-4">
-                <label
-                  className="block text-green-700 text-sm font-bold mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-              </div>
-
-              {errorPassword}
-
-              <input
-                className="bg-green-800 w-full mt-5 p-2 text-white uppercase hover:bg-green-900"
-                type="submit"
-                value="Create Account"
-              />
-            </form>
+            <NewAccountForm formik={formik}/>
           </div>
         </div>
+        
       </Layout>
     </>
   );
