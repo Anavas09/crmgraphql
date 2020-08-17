07fa610b9260e2e3493c155dcccce01e0dab7209
Angel Navas <angelnavasramos@gmail.com>
2020-08-17 16:33:01 -0400

    Add Formik and Yup for validations and display errros in the login page. Add AUTH_USER mutation for authenticate users and show message errors if we have.

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,15 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 
+const AUTH_USER = gql`
+  mutation authUser($input: AuthUserInput) {
+    authUser(input: $input) {
+      token
+    }
+  }
+`;
+
 function Login() {
+  const [message, setMessage] = useState(null);
+  const [good, setGood] = useState(false);
+  
+  //Next.js routing
+  const router = useRouter();
+
+  //Login user Mutation
+  const [authUser] = useMutation(AUTH_USER);
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid Email')
+        .required("Email can't be empty"),
+      password: Yup.string().required('Password required'),
+    }),
+    onSubmit: async values => {
+      const { email, password } = values;
+      console.log(values);
+
+      try {
+
+        const { data } = await authUser({
+          variables: {
+            input: {
+              email,
+              password
+            }
+          }
+        });
+        
+        console.log(data);
+        setGood(true);
+        setMessage('Login...');
+        
+        const { token } = data.authUser;
+
+        window.localStorage.setItem('token', token);
+
+        setTimeout(() => {
+          setMessage(null);
+          setGood(false);
+
+          //Redirect to client page
+          router.push('/');
+        }, 2000);
+
+
+      } catch (err) {
+        setMessage(err.message);
+        console.error(err.message);
+        setTimeout(() => {
+          setMessage(null);
+        }, 3000);
+      };
+    },
+  });
+
+  const errorEmail = formik.touched.email && formik.errors.email && (
+    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+      <p className="font-bold">{formik.errors.email}</p>
+    </div>
+  );
+
+  const errorPassword = formik.touched.password && formik.errors.password && (
+    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+      <p className="font-bold">{formik.errors.password}</p>
+    </div>
+  );
+
+  const showMessage = () => {
+    return good ? (
+      <div className="bg-green-200 text-green-700 font-bold py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{message.toUpperCase()}</p>
+      </div>
+    ) : (
+      <div className="bg-red-200 text-red-700 font-bold py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{message.toUpperCase()}</p>
+      </div>
+    );
+  };
+
   return (
     <>
       <Layout>
+
+        {message && showMessage()}
+
         <h1 className="text-center text-2xl text-white font-light">Login</h1>
 
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+            <form
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
               <div className="mb-4">
                 <label
                   className="block text-green-700 text-sm font-bold mb-2"
@@ -22,8 +127,14 @@ function Login() {
                   id="email"
                   type="email"
                   placeholder="User Email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
                 />
               </div>
+
+              {errorEmail}
+
               <div className="mb-4">
                 <label
                   className="block text-green-700 text-sm font-bold mb-2"
@@ -36,8 +147,14 @@ function Login() {
                   id="password"
                   type="password"
                   placeholder="Password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
                 />
               </div>
+
+              {errorPassword}
+
               <input
                 className="bg-green-800 w-full mt-5 p-2 text-white uppercase hover:bg-green-900"
                 type="submit"
