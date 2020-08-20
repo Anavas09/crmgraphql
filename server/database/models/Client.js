7abbc5a4860847d7f1467211f255294f16e2806a
Angel Navas <angelnavasramos@gmail.com>
2020-08-20 15:45:23 -0400

    Change lastName (camelCase) to lastname (lowercase) in the Clients model (MongoDB Document).

--- server/database/models/Client.js
+++ server/database/models/Client.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const ClientSchema = new Schema({
   name: { type: String, required: true, trim: true },
-  lastName: { type: String, required: false, trim: true },
+  lastname: { type: String, required: false, trim: true },
   email: { type: String, required: true, trim: true, unique: true },
   company: { type: String, required: true, trim: true },
   phone: { type: String, required: false, trim: true },
