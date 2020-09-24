fbf2caf8527610560502f41bd1e7c3af3d6e5ba2
Angel Navas <angelnavasramos@gmail.com>
2020-09-24 18:40:25 -0400

    Production.

--- server/database/config/db.js
+++ server/database/config/db.js
@@ -4,24 +4,23 @@ require("dotenv").config({
   path: "variables.env",
 });
 
+const mongoDB_URI = process.env.PORT !== 4000 ? process.env.DB_MONGO_URI: process.env.LOCAL_DB_MONGO_URI
+
 const connectDB = async () => {
   try {
     await mongoose
-      .connect(process.env.DB_MONGO_URI, {
+      .connect(mongoDB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
       })
-      .then(db => {
-        db.connection.db.listCollections().toArray((err, names) => {
-          console.log(names);
-        });
+      .then(() => {
         console.info("DB is connected");
       })
       .catch(err => console.error(err));
   } catch (err) {
-    console.error("AN ERROR");
+    console.error("SOMETHING WRONG WAS HAPPEND: ERROR");
     console.error(err);
     process.exit(1);
   }
